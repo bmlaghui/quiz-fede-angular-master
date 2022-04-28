5af0358847f6b3887ad7252cf41efdb9639c97ed
bmlaghui <bmlaghui@gmail.com>
2022-04-28 20:27:53 +0200

    using api nodejs deployed on herokou

--- src/app/service/question.service.ts
+++ src/app/service/question.service.ts
@@ -9,7 +9,7 @@ export class QuestionService {
   constructor(private http:HttpClient) { }
   private data:any = []
   getQuestionFromJson(classe:String, matiere:String) {
-    const url ="http://localhost:8080/questionsQUIZ/"+matiere+"/10"
+    const url ="https://qcmapi.herokuapp.com/questionsQUIZ/"+matiere+"/20"
 
 
     this.http.get(url).subscribe((res)=>{
